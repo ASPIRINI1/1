e09190e5ce0b9f6f184609b7c0f5e61e7e8e2314
ASPIRINI1 <zverk3333@gmail.com>
2022-10-19 00:38:25 +0300

    BannerController changed from View to TableView controller.

--- 1/MainModule/Banners/BannerController.swift
+++ 1/MainModule/Banners/BannerController.swift
@@ -11,51 +11,45 @@ protocol BannerControllerDelegate: AnyObject {
     func bannerController(_ collectionView: UICollectionView, didSelectItemAtIndex: Int)
 }
 
-class BannerController: UIViewController {
+class BannerController: UICollectionViewController {
     
-    lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: 300, height: 100)
-        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(BannerCell.self, forCellWithReuseIdentifier: BannerCell.identefier)
-        collectionView.frame.size = CGSize(width: view.frame.width, height: 120)
-        collectionView.showsHorizontalScrollIndicator = false
-        collectionView.backgroundColor = .clear
-        view = collectionView
-        return collectionView
-    }()
     var banners: [UIImage]!
     weak var delegate: BannerControllerDelegate?
     
     init(banners: [UIImage]) {
-        super.init(nibName: nil, bundle: nil)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 300, height: 120)
+        super.init(collectionViewLayout: layout)
         self.banners = banners
+        view = collectionView
+        collectionView.register(BannerCell.self, forCellWithReuseIdentifier: BannerCell.identefier)
+        collectionView.frame.size = CGSize(width: view.frame.width, height: 120)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func viewDidLoad() {
-        view.addSubview(collectionView)
-    }
 }
 
-extension BannerController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+// MARK: - UICollectionViewDelegate
+
+extension BannerController {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         delegate?.bannerController(collectionView, didSelectItemAtIndex: indexPath.row)
     }
 }
 
-extension BannerController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+// MARK: - UICollectionViewDataSource
+
+extension BannerController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return banners.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BannerCell.identefier, for: indexPath) as? BannerCell {
             cell.fill(image: banners[indexPath.row])
             return cell
